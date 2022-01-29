989a99e74325a2931171f127a27f1526499f4009
Felipe <lipeigansi@gmail.com>
2022-01-28 22:59:09 -0300

    Aula 05 - Conclusão

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-
   constructor(props) {
     super(props);
     this.titulo = "";
     this.texto = "";
-    this.categoria = "Sem categoria";
+    this.categoria = "Sem Categoria";
+    this.state = { categorias: [] }
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
 
-  _handleMudancaTitulo(evento) {
-    evento.stopPropagation();
-    this.titulo = evento.target.value;
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias);
   }
-  
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
+
+  }
+  _novasCategorias(categorias) {
+    this.setState({ ...this.state, categorias })
+  }
+
   _handleMudancaCategoria(evento) {
     evento.stopPropagation();
     this.categoria = evento.target.value;
+  }
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
   }
 
   _handleMudancaTexto(evento) {
@@ -28,22 +39,20 @@ class FormularioCadastro extends Component {
     evento.preventDefault();
     evento.stopPropagation();
     this.props.criarNota(this.titulo, this.texto, this.categoria);
-
   }
 
   render() {
     return (
-      <form className="form-cadastro"
-        onSubmit={this._criarNota.bind(this)} >
-        <select 
-        onChange={this._handleMudancaCategoria.bind(this)}
-        className="form-cadastro_input">
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
           <option>Sem Categoria</option>
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>
 
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>;
           })}
-          
         </select>
         <input
           type="text"
